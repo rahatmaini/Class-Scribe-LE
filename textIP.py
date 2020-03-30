62208f0f513b43f527f8f6531260657e8ce3dee5
Rahat Maini <rahatmaini@live.com>
2020-03-30 08:57:57 -0400

    Went back in time bitch

--- textIP.py
+++
@@ -1,7 +0,0 @@
-from twilio.rest import Client
-import os
-
-def textIPtoRahat(msg):
-    client = Client("AC09e045976b218be8ae4b29a37465b9be", os.environ.get("TWILIO"))
-    client.messages.create(to="+14077660001", from_="+14195498636", body=str(msg))
-    return True # if failed then wouldthrow error
